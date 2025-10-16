753398bf4ce5418d78198bf4c9d62d1bb4fa5e40
Vanesa <57409926+vanesa33@users.noreply.github.com>
2025-10-16 17:52:52 -0300

    Update axios base URL to include '/api' path

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@
 // Usa la variable de entorno si existe, o localhost si estás en modo dev
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL 
-    ? `${import.meta.env.VITE_API_URL}`
+    ? `${import.meta.env.VITE_API_URL}/api`
     : 'http://localhost:4000/api',
   withCredentials: true,
 });
