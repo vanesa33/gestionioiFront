3cf2e97f8f31863deac903c186f65a87ff8dacb9
Vanesa <57409926+vanesa33@users.noreply.github.com>
2025-10-16 18:00:31 -0300

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,7 +3,7 @@ import axios from './axios';
 
 export const registerRequest = async (user) => axios.post(`/register`, user);
 
-export const loginRequest =  async (user) => axios.post(`/login`,  user);
+export const loginRequest =  async (data) => axios.post(`/login`,  data);
 
 
 export const verifyTokenRequest = async () => axios.post(`/verify`);
