b9742a4f0e0f4d30d89323a34d6953aa0b6a71db
Vanesa <57409926+vanesa33@users.noreply.github.com>
2025-11-13 20:30:47 -0300

    Fix logo image path in LoginPage component

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -39,7 +39,7 @@ if (isAuthenticated) navigate("/");
   {/* Lado izquierdo - Logo y texto */}
   <div className="w-1/2 flex flex-col justify-center items-center text-white p-8">
  
-    <img src="../src/img/logo-ioi.jpeg" alt="Logo" className="w-40 mb-4" />
+    <img src="/logo-ioi.jpeg" alt="Logo" className="w-40 mb-4" />
     <h1 className="text-2xl font-bold">Instalaciones Odontológicas Integrales</h1>
     <p className="text-sm mt-2">SISTEMA DE TKT</p>
   </div>
