f79d2ef55c8f31fa9eff9613f08383c8cb598aa4
Vanesa <57409926+vanesa33@users.noreply.github.com>
2025-12-03 21:07:10 -0300

    Update TasksBuscarOrden.jsx

--- src/pages/TasksBuscarOrden.jsx
+++ src/pages/TasksBuscarOrden.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useTasks } from "../context/useTasks";
 import { useNavigate } from "react-router-dom";
 import { getTodosIngresosRequest } from "../api/ingresos";
-import { imprimirOrdenBuscar } from "../imprimirOrdenBuscar.js";
+import { imprimirOrdenBuscarUno } from "../imprimirOrdenBuscarUno.js";
 import * as XLSX from "xlsx";
 
 
