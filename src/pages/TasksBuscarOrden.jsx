8c1d4e36578216aa96df245847e2e0f54021ea32
Vanesa <57409926+vanesa33@users.noreply.github.com>
2025-12-05 16:29:37 -0300

    Update TasksBuscarOrden.jsx

--- src/pages/TasksBuscarOrden.jsx
+++ src/pages/TasksBuscarOrden.jsx
@@ -147,7 +147,7 @@ function TasksBuscarOrden() {
 
              
       <button
-         onClick=() => exportarIngresosToExcel (ingresos)}
+         onClick={() => exportarIngresosToExcel (ingresos)}
        className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700 shadow mb-3"
        >
   📥 Descargar Excel
