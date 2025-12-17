0825f42afb77d32a5622354ae5f508006dd2b400
Vanesa <57409926+vanesa33@users.noreply.github.com>
2025-12-17 12:28:45 -0300

    Update TasksBuscarOrden.jsx

--- src/pages/TasksBuscarOrden.jsx
+++ src/pages/TasksBuscarOrden.jsx
@@ -379,7 +379,7 @@ function TasksBuscarOrden() {
               </button>
               <button
                 onClick={() => {
-                  imprimirIngreso(ordenSeleccionada);
+                  imprimirOrdenBuscarUno(ordenSeleccionada);
                 }}
                 className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
               >
