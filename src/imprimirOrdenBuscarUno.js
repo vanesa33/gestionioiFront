0c89c05bfa4ca8fceff42b10b80e5279f4b032f9
Vanesa <57409926+vanesa33@users.noreply.github.com>
2025-12-26 15:11:10 -0300

    Update imprimirOrdenBuscarUno.js

--- src/imprimirOrdenBuscarUno.js
+++ src/imprimirOrdenBuscarUno.js
@@ -161,7 +161,8 @@ export function imprimirOrdenBuscarUno(orden) {
     <p><strong>Repuesto:</strong> ${orden.repuesto}</p>
     <p><strong>Mano de Obra:</strong> ${orden.manoobra}</p>
     <p><strong>Total:</strong> ${orden.total}</p>
-    <p><strong>Salida </strong> ${orden.salida}</p>
+    <p><strong>Salida:</strong> ${formatearFecha(orden.salida)}</p>
+
    
      
   </div>
@@ -185,6 +186,7 @@ export function imprimirOrdenBuscarUno(orden) {
   ventana.document.close();
   ventana.onload = () => ventana.print();
 }
+
 
 
 
