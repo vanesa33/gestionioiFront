65fcaad68abfd673f2c6032732856c18d3e9002b
Vanesa <57409926+vanesa33@users.noreply.github.com>
2026-01-28 16:18:32 -0300

    Update users.js

--- src/api/users.js
+++ src/api/users.js
@@ -1,26 +1,18 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-    ? import.meta.env.VITE_API_URL
-    : "http://localhost:4000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000",
   withCredentials: true,
 });
 
 // traer usuarios
-export const getUsersRequest = () =>
-  api.get("/passuser");
-
-
-
+export const getUsersRequest = () => api.get("/passuser");
 
 // admin → reset password
-export const resetPasswordRequest = ( userId) => {
+export const resetPasswordRequest = (userId) => {
   if (!userId) throw new Error("User ID is required for resetting password");
   return api.put(`/passuser/${userId}/reset-password`);
-}
-  //api.put(`/passuser/reset-password`, data);
-
+};
 
 // usuario logueado → cambiar su propia password
 export const cambiarPasswordRequest = (data) =>
