5caa93e704292af16f4372bfb2a2f1eda4dcc2f8
Vanesa <57409926+vanesa33@users.noreply.github.com>
2026-01-30 16:10:20 -0300

    Update tasks.js

--- src/api/tasks.js
+++ src/api/tasks.js
@@ -2,13 +2,21 @@ import axios from './axios';
 
 ////table Client ///
 
-export const getTasksRequest = async () => axios.get("/tasks");
-export const getTaskRequest = async (id) => axios.get(`/tasks/${id}`);
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL 
+    ? import.meta.env.VITE_API_URL
+    : "http://localhost:4000",
+  withCredentials: true,
+})
 
-export const createTasksRequest = async (task) => axios.post("/tasks/", task);
 
-export const upDateTasksRequest = async (id, task) => axios.put(`/tasks/${id}`, task);
-export const deleteTasksRequest = async (id) => axios.delete(`/tasks/${id}`);
+export const getTasksRequest = async () => api.get("/tasks");
+export const getTaskRequest = async (id) => api.get(`/tasks/${id}`);
+
+export const createTasksRequest = async (task) => api.post("/tasks/", task);
+
+export const upDateTasksRequest = async (id, task) => api.put(`/tasks/${id}`, task);
+export const deleteTasksRequest = async (id) => api.delete(`/tasks/${id}`);
 
 
 
