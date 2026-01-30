6ed34adb96fb1c0cf3e99ed17fd0ef8b236bff53
Vanesa <57409926+vanesa33@users.noreply.github.com>
2026-01-30 15:40:19 -0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,7 +4,7 @@
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_URL 
     ? `${import.meta.env.VITE_API_URL}`
-    : 'http://localhost:4000/api',
+    : 'http://localhost:4000',
   withCredentials: true,
 });
 
