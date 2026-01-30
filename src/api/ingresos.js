40f2f392dcfec6b5c1ecb823cb62efff5918c9f4
Vanesa <57409926+vanesa33@users.noreply.github.com>
2026-01-30 16:08:35 -0300

    Update ingresos.js

--- src/api/ingresos.js
+++ src/api/ingresos.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const api = axios.create({
      baseURL: import.meta.env.VITE_API_URL 
     ? `${import.meta.env.VITE_API_URL}`
-    : 'http://localhost:4000/api',
+    : 'http://localhost:4000',
     withCredentials: true
 })
 
