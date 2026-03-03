de8c9986854a66e6e3f24930ebdcda8cba41cfd4
Vanesa <57409926+vanesa33@users.noreply.github.com>
2026-03-03 16:02:47 -0300

    Update TasksBuscarOrden.jsx

--- src/pages/TasksBuscarOrden.jsx
+++ src/pages/TasksBuscarOrden.jsx
@@ -226,7 +226,7 @@ const esteMes = () => {
 
   return (
     
-    <div className="bg-gray-200 min-h-screen px-4 md:px-8 py-6">
+    <div className="bg-gray-200 min-h-screen px-2 md:px-8 py-6">
       <div className="max-w-7xl mx-auto">
       <div>
       <h1 className="text-2xl text-gray-600 font-bold mb-4">Buscar Orden Técnica</h1>
