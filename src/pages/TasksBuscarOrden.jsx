71f339c1a1d86fa2490dba553dd846739a303fa2
Vanesa <57409926+vanesa33@users.noreply.github.com>
2026-03-20 17:49:11 -0300

    Update print button to use imprimirOrdenBuscarUno

--- src/pages/TasksBuscarOrden.jsx
+++ src/pages/TasksBuscarOrden.jsx
@@ -667,7 +667,7 @@ const esteMes = () => {
 
   {/* IMPRIMIR */}
   <button
-    onClick={() => imprimirIngreso(ordenSeleccionada)}
+    onClick={() => imprimirIngreso(imprimirOrdenBuscarUno)}
     className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
   >
     Imprimir
