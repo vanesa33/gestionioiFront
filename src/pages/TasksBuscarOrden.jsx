ee3bf2833d4d779f49ec46762d09c321c0a2e940
Vanesa <57409926+vanesa33@users.noreply.github.com>
2026-03-31 17:45:15 -0300

    Update TasksBuscarOrden.jsx

--- src/pages/TasksBuscarOrden.jsx
+++ src/pages/TasksBuscarOrden.jsx
@@ -667,7 +667,7 @@ const esteMes = () => {
 
   {/* IMPRIMIR */}
   <button
-    onClick={() => imprimirOrdenBuscarUno(imprimirOrdenBuscarUno)}
+    onClick={() => imprimirOrdenBuscarUno(ordenSeleccionada)}
     className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
   >
     Imprimir
