51c773c5baf21187bdbac8ed737a3cd5c376897b
Vanesa <57409926+vanesa33@users.noreply.github.com>
2026-06-10 16:51:47 -0300

    Update EditarOrden.jsx

--- src/pages/EditarOrden.jsx
+++ src/pages/EditarOrden.jsx
@@ -2,6 +2,8 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { getIngresoRequest, upDateingresoRequest, deleteIngresoRequest } from "../api/ingresos";
+import { getTecnicosRequest } from "../api/users.js";
+
 
 
 function EditarOrden() {
@@ -11,12 +13,31 @@ function EditarOrden() {
   const [numOrden, setNumOrden] = useState(iid);
   const [ordenCerrada, setOrdenCerrada] = useState(false);
   const [tipoOrden, setTipoOrden] = useState("");
+   const [tecnicos, setTecnicos] = useState([]);
+
+   
+  useEffect(() => {
+  const cargarTecnicos = async () => {
+    try {
+      const res = await getTecnicosRequest();
+      setTecnicos(res.data.result || []);
+      
+    } catch (error) {
+      console.error("Error cargando técnicos:", error);
+      setTecnicos([]);
+    }
+  };
+
+  cargarTecnicos();
+}, [reset]);
+
 
   useEffect(() => {
     const cargarDatos = async () => {
       try {
         const res = await getIngresoRequest(iid);
         const data = res.data;
+        console.log( data.tecnico_nombre);    
 
         reset({
           equipo: data.equipo ?? "",
@@ -32,7 +53,10 @@ function EditarOrden() {
           presu: data.presu ?? "",
           salida: data.salida ? data.salida.split("T")[0] : "",
           imagenurl: data.imagenurl ?? "",
-          client_id: data.client_id ?? ""
+          client_id: data.client_id ?? "",
+          usuario_nombre: data.usuario_nombre ?? "",
+          tecnico_id: data.tecnico_id ?? "",
+          tecnico_nombre: data.tecnico_nombre ?? ""
         });
 
         setTipoOrden(data.tipo_orden);
@@ -49,6 +73,8 @@ function EditarOrden() {
 
     cargarDatos();
   }, [iid, reset]);
+
+  
 
   // Escuchar valores
 const costo = watch("costo") || 0;
@@ -121,16 +147,16 @@ const handleDelete = async () => {
 
 
   return (
-    <div className="max-w-2xl mx-auto p-6 bg-white shadow-lg rounded-lg mt-10">
+    <div className="max-w-6xl mx-auto p-6  bg-gray-200 shadow-lg rounded-lg mt-10">
       <div className="text-center mb-6">
   <span
     className={`inline-block px-3 py-1 rounded-full text-xs font-bold mb-2
-      ${tipoOrden === "SERVICE"
+      ${tipoOrden?.toUpperCase() === "SERVICE"
         ? "bg-blue-100 text-blue-800"
         : "bg-green-100 text-green-800"}
     `}
   >
-    {tipoOrden === "SERVICE" ? "🔧 SERVICE" : "🛠 REPARACIÓN"}
+    {tipoOrden?.toUpperCase() === "SERVICE" ? "🔧 SERVICE" : "🛠 REPARACIÓN"}
   </span>
 
   <h2 className="text-2xl font-bold text-gray-700 mt-2">
@@ -143,9 +169,25 @@ const handleDelete = async () => {
           ⚠️ Esta orden está cerrada y no puede ser editada.
         </div>
       )}
-
+          
+      <div className="mb-6">
+        <p className="text-gray-600">
+          Edita los detalles de la orden técnica. Recuerda que una vez que se
+          ingresa una fecha de salida, la orden se considera cerrada y no podrá
+          ser modificada.
+        </p>
+      </div>
+      <div className="border-t pt-4">
+        <p className="text-sm text-gray-500">
+          orden creada por: <b>{watch("usuario_nombre")}</b> {/* Este valor ya estará en el form si lo envía el backend */}
+        </p>
+        </div>
       <form onSubmit={handleSubmit(onSubmit)} 
-      className={`space-y-4 ${ordenCerrada ? "pointer-events-none opacity-70" : ""}`}>
+      className={`grid grid-cols-1 md:grid-cols-2 gap-4
+        ${ordenCerrada ? "pointer-events-none opacity-70" : ""}`}>
+ 
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
+
         <div>
           <label className="block font-semibold text-gray-600">Equipo</label>
           <input
@@ -153,35 +195,6 @@ const handleDelete = async () => {
             {...register("equipo")}
             className="w-full border border-gray-300 p-2 rounded text-gray-600"
             placeholder="Ingrese el equipo"
-          />
-        </div>
-
-        <div>
-          <label className="block font-semibold text-gray-600">Falla</label>
-          <input
-            type="text"
-            {...register("falla")}
-            className="w-full border border-gray-300 p-2 rounded text-gray-600"
-            placeholder="Ingrese la falla"
-          />
-        </div>
-
-        <div>
-          <label className="block font-semibold text-gray-600">Observaciones</label>
-          <input
-            type="text"
-            {...register("observa")}
-            className="w-full border border-gray-300 p-2 rounded text-gray-600"
-            placeholder="Observaciones"
-          />
-        </div>
-
-        <div>
-          <label className="block font-semibold text-gray-600">Fecha</label>
-          <input
-            type="date"
-            {...register("fecha")}
-            className="w-full border border-gray-300 p-2 rounded text-gray-600"
           />
         </div>
 
@@ -195,21 +208,65 @@ const handleDelete = async () => {
           />
         </div>
 
-        <label className="block font-semibold text-gray-600">Garantía</label>
+        <div>
+          <label className="block font-semibold text-gray-600">Falla</label>
+          <input
+            type="text"
+            {...register("falla")}
+            className="w-full border border-gray-300 p-2 rounded text-gray-600"
+            placeholder="Ingrese la falla"
+          />
+        </div>
 
+       <div>
+          <label className="block font-semibold text-gray-600">Fecha</label>
+          <input
+            type="date"
+            {...register("fecha")}
+            className="w-full border border-gray-300 p-2 rounded text-gray-600"
+          />
+        </div>
+
+
+
+         </div>
+
+        <div className="md:col-span-2">
+          <label className="block font-semibold text-gray-600">Observaciones</label>
+          <input
+            type="text"
+            {...register("observa")}
+            className="w-full border border-gray-300 p-7 rounded text-gray-600"
+            rows={8}
+            placeholder="Observaciones"
+          />
+        </div>    
+
+        
+
+        
+       <div className="flex items-center gap-4">
+        
+        <label className="block font-semibold text-gray-600">Garantía</label>
+           
           <select className="p-2 rounded border block font-semibold text-gray-600"  {...register("presu")}>
                   <option value="Sí">Sí</option>
                   <option value="No">No</option>
                 </select>
 
-        <div>
+        
            {esGarantia && (
           <div className="p-2 mb-2 rounded bg-blue-100 text-blue-800 text-sm font-semibold">
-             🔧 Orden en garantía — no se cobran costos
+             🔧 Orden en garantía — no se cobra mano de obra
             </div>
           )}
+         </div>
          
-        </div>
+         <div></div>
+
+         <div className="bg-white rounded-xl shadow p-5">
+        
+ <div className="grid grid-cols-2 gap-4">
 
          <div>
           <label className="block font-semibold text-gray-600">Costo de Repuesto</label>
@@ -217,7 +274,7 @@ const handleDelete = async () => {
             type="number"
             {...register("repuesto")}
             className={`w-full border p-2 rounded text-gray-600
-               ${esGarantia ? "bg-gray-100 cursor-not-allowed" : ""}
+               ${esGarantia ? "bg-gray-100 " : ""}
               `}
             placeholder="$"
           />
@@ -244,18 +301,22 @@ const handleDelete = async () => {
             
           <label className="block font-semibold text-gray-600">IVA</label>
            <select className={`p-2 rounded border font-semibold
-    ${esGarantia ? "bg-gray-100 cursor-not-allowed" : "text-gray-600"}
-  `}
+            ${esGarantia ? "bg-gray-100 cursor-not-allowed" : "text-gray-600"}
+  `           }
   
-            {...register("iva")}>
+                  {...register("iva")}>
                   <option value="Sí">Sí</option>
                   <option value="No">No</option>
-                </select>
+                  </select>
         
-<input type="hidden" {...register("montoIva")} />
-<input type="hidden" {...register("totalSinIva")} />
+                <input type="hidden" {...register("montoIva")} />
+                <input type="hidden" {...register("totalSinIva")} />
 
- <div>
+          </div>
+
+          </div>
+
+          <div>
           <label className="block font-semibold text-gray-600">Total</label>
           <input
             type="number"
@@ -266,8 +327,19 @@ const handleDelete = async () => {
             readOnly
             placeholder="$"
           />
+          </div>
+
+
+        
+        <div></div>
+        <div>
+
+
+ 
+        
         </div>
 
+<div className="md:col-span-3">
         <div className="mt-4 p-3 border rounded bg-gray-50">
   <p className="font-semibold text-gray-700">
     Total sin IVA: <span className="font-bold">${watch("totalSinIva")}</span>
@@ -283,11 +355,40 @@ const handleDelete = async () => {
     Total Final: <span>${watch("total")}</span>
   </p>
 </div>
+</div>
+      </div>
 
-      
-                </div>
+</div>
+
+       <div className="bg-white rounded-xl shadow p-5">
+               
+        <div className=" p-3 border rounded bg-gray-200  ">
+          
+          <p className="text-sm text-gray-500">Técnico asignado: <b>{
+            
+            tecnicos.find(t => t.ruid === Number(watch("tecnico_id")))?.username || "No asignado"
+            
+            }</b></p>
+           </div>
+              
+
+          
         
-        </div>
+  <select 
+  {...register("tecnico_id")}
+  className="border p-2 text-gray-600 rounded mb-3"
+  >
+   <option value="">Seleccionar Técnico</option>
+   {Array.isArray(tecnicos) &&
+  tecnicos.map((t) => (
+    <option key={t.ruid} value={t.ruid}>
+      {t.username}
+    </option>
+  ))
+}  
+  </select>
+  
+ 
 
          <div>
           <label className="block font-semibold text-gray-600">Fecha de Salida / Cerrar Orden</label>
@@ -313,7 +414,7 @@ const handleDelete = async () => {
         Tipo de orden:{" "}
          <span
         className={`font-bold
-             ${tipoOrden === "SERVICE"
+             ${tipoOrden === ""
           ? "text-blue-700"
            : "text-green-700"}
             `}
@@ -335,7 +436,9 @@ const handleDelete = async () => {
           </span>
         </div>
 
-        <div className="flex justify-between mt-6 pointer-events-auto">
+        </div>
+
+        <div className="lg:col-span-2 flex justify-center gap-4 mt-4">
           <button
             type="submit"
             disabled={ordenCerrada}
@@ -378,3 +481,6 @@ const handleDelete = async () => {
 }
 
 export default EditarOrden;
+
+  
+  
