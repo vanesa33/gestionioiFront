8e369242f38a47fc05385bfb92efd318c7a7bac6
Vanesa <57409926+vanesa33@users.noreply.github.com>
2026-06-16 14:23:01 -0300

    Update TasksBuscarOrden.jsx

--- src/pages/TasksBuscarOrden.jsx
+++ src/pages/TasksBuscarOrden.jsx
@@ -709,7 +709,7 @@ console.log("orden seleccionada", orden);
 
   {/* IMPRIMIR */}
   <button
-    onClick={() => imprimirIngreso(ordenSeleccionada)}
+    onClick={() => imprimirOrdenBuscarUno(ordenSeleccionada)}
     className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
   >
     Imprimir
