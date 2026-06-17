30c2d5888e2a5f8ca1eeba590333164492272305
Vanesa <57409926+vanesa33@users.noreply.github.com>
2026-06-17 13:45:27 -0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -25,7 +25,7 @@ function HomePage() {
      <a
   href={manualUsuario}
   download="Manual_Usuario_IOI.pdf"
-  className="text-gray-700 hover:text-white p-5 w-40"
+  className="text-gray-700 hover:text-white p-3 w-60"
 >
   📘 Descargar Manual Usuario
 </a>
