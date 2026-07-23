c21ef8444d3fbfa8953e3e6eab3aa4b41729fcc6
Vanesa <57409926+vanesa33@users.noreply.github.com>
2026-07-23 14:35:44 -0300

    Add user support message in LoginPage
    Added a message for users experiencing access issues.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -124,7 +124,10 @@ if (isAuthenticated) navigate("/");
           className='text-red-900'><b>Register</b></Link>
         </p>
 
-        
+        <p className="text-sm text-gray-500 mt-4 text-center">
+            Si experimenta algún inconveniente para acceder o utilizar la aplicación,
+            comuníquese con el administrador del sistema o con la persona responsable de la empresa.
+        </p>
 
         </div>
         </div>
